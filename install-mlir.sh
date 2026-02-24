355ba0920328ee3f89430adae7610334d6411cc8
Christophe Guillon <christophe.guillon@inria.fr>
2026-02-24 12:35:51 +0100

    Update install mlir version/repo

--- install-mlir.sh
+++ install-mlir.sh
@@ -4,8 +4,8 @@ set -x
 dir="$(dirname "$(readlink -f "$0")")"
 
 PYTHON="${PYTHON-python}"
-INDEX_URL=https://test.pypi.org/simple
-MLIR_VERSION=21.1.2.5rc1
+INDEX_URL=https://pypi.org/simple
+MLIR_VERSION=21.1.2.5
 
 $PYTHON -m pip install \
         "xtc-mlir-tools==$MLIR_VERSION" \
