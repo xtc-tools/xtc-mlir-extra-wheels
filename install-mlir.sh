4bc408c237f85f11ab99973124e77fb92109cb26
Christophe Guillon <christophe.guillon@inria.fr>
2026-02-24 15:37:59 +0100

    Update llvm/mlir version to 21.1.2.6

--- install-mlir.sh
+++ install-mlir.sh
@@ -5,7 +5,7 @@ dir="$(dirname "$(readlink -f "$0")")"
 
 PYTHON="${PYTHON-python}"
 INDEX_URL=https://pypi.org/simple
-MLIR_VERSION=21.1.2.5
+MLIR_VERSION=21.1.2.6
 
 $PYTHON -m pip install \
         "xtc-mlir-tools==$MLIR_VERSION" \
